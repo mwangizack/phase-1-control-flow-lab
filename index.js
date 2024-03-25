53a8c8476d3f4cdabb893e804060a3d8142908b2
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-03-25 21:43:19 +0300

    Add conditionals to the functions

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  if (distanceInFeet <= 400){
+  return 'This one is on me!';
+  }
+  else if (distanceInFeet > 400 && distanceInFeet <= 2000){
+    return 'That will be twenty bucks.'
+  }
+  else if (distanceInFeet > 2000 && distanceInFeet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destinationCity){
   // Write your code here!
+  return destinationCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
